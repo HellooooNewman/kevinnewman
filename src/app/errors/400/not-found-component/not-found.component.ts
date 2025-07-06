90c40943ad841b25aa3008deac92fd8519b30def
Kevin Newman <kevin@kevinnewman.ca>
2025-07-06 13:23:22 -0400

    Fixing translate error

--- src/app/errors/400/not-found-component/not-found.component.ts
+++ src/app/errors/400/not-found-component/not-found.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: 'not-found.component.html',
-  styleUrls: ['not-found.component.scss']
+  styleUrls: ['not-found.component.scss'],
+  imports: [TranslateModule],
 })
 export class NotFoundComponent {
 
